edf4c9b7fd34fbb1e153a39304ed390c86b502a8
Pedro Pacheco <up201806824@fe.up.pt>
2020-04-13 16:51:55 +0000

    Updated MyCylinder

--- WebCGF2.proj/proj/MyCylinder.js
+++ WebCGF2.proj/proj/MyCylinder.js
@@ -7,37 +7,43 @@ class MyCylinder extends CGFobject {
 
     constructor(scene, slices) {
         super(scene);
-        this.sides = slices;
+        this.slices = slices;
         this.initBuffers();
       }
 
     initBuffers() {
+        //Arrays needded to store the vertices, indices, normals and texture coordinates
 		this.vertices = [];
         this.indices = [];
         this.normals = [];
-        this.texCoords = [];
+        this.textCoords = [];
 
+        //Angles needded to calculate the vertices, normals and texture coordinates
         var ang = 0;
         var alpha = 2*Math.PI/this.slices;
 
-        for(var i = 0; i < this.slices; i++) {
-            this.vertices.push(Math.cos(ang), 0, -Math.sin(ang));
+        //Adds the basic vertices, normals, and texture coordinates
+        this.vertices.push(1, 0, 0);
+        this.vertices.push(1, 3, 0);
+        this.normals.push(1, 0, 0);
+        this.normals.push(1, 0, 0);
+        this.textCoords.push(alpha, 0);
+        this.textCoords.push(alpha, 1);
+
+        //Calculates and adds the vertices, normals, and texture coordinates
+        //according to the alpha angle 'alpha' 
+        for(var i = 0; i < this.slices; i++){
+            this.vertices.push(Math.cos(ang), 3, -Math.sin(ang));
             this.vertices.push(Math.cos(ang), 0, -Math.sin(ang));
             this.normals.push(Math.cos(ang), 0, -Math.sin(ang));
             this.normals.push(Math.cos(ang), 0, -Math.sin(ang));
-            this.texCoords.push(ang/(2*Math.PI),0);
-            this.texCoords.push(ang/(2*Math.PI),0);
-            ang+=alpha;
+            this.textCoords.push(ang/(2*Math.PI),0);
+            this.textCoords.push(ang/(2*Math.PI),0);
+            ang += alpha;
         }
 
-        this.vertices.push(1, 0, 0);
-        this.vertices.push(1, 1, 0);
-        this.normals.push(1, 0, 0);
-        this.normals.push(1, 0, 0);
-        this.texCoords.push(alpha, 0);
-        this.texCoords.push(alpha, 1);
-
-        for(var i = 0; i < this.slices; i++) {
+        //Calculates the indices
+        for(var i = 0; i < this.slices; i++){
             this.indices.push(2*i, 2*i+2, 2*i+1);
             this.indices.push(2*i+1, 2*i+2, 2*i+3);
         }
@@ -45,18 +51,5 @@ class MyCylinder extends CGFobject {
         this.primitiveType = this.scene.gl.TRIANGLES;
         this.initGLBuffers();
     
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 }
