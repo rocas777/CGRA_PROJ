391b5727115b6f4baa0aa1a08b440ed0219616f0
Pedro Pacheco <up201806824@fe.up.pt>
2020-04-20 18:44:29 +0100

    Vehicle updates

--- proj/MyVehicle.js
+++ proj/MyVehicle.js
@@ -3,10 +3,21 @@
 * @constructor
 */
 class MyVehicle extends CGFobject {
-    constructor(scene) {
+    constructor(scene, deltaY, initSpeed, xPos, yPos, zPos) {
         super(scene);
         this.pyramid = new MyPyramid(scene, 4, 10);
+        this.deltaY = deltaY;
+        this.initSpeed = initSpeed;
+        this.speed = 0;
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.zPos = zPos;
     }
+
+    update(){
+
+    }
+
 
     display() {
         var rotationMatrix = [
@@ -17,6 +28,7 @@ class MyVehicle extends CGFobject {
         ];
         this.scene.pushMatrix();
         this.scene.multMatrix(rotationMatrix);
+        this.scene.translate(this.xPos, this.yPos, this.zPos);
         this.pyramid.display();
         this.scene.popMatrix();
     }
